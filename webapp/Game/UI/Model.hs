e801376987a9d614b53d05eb4e4ce69b597d77cb
Kevin Horlick <KevinHorlick@gmail.com>
2025-05-04 12:25:34 -0400

    ui for HitCard

--- webapp/Game/UI/Model.hs
+++ webapp/Game/UI/Model.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE StrictData #-}
 
 module Game.UI.Model (module Game.UI.Model) where
@@ -25,4 +26,7 @@ update :: Decision -> Model -> Model
 update msg model = traceShow ("update", msg, model) $ case msg of
   Right (DealingEvt (CardsDealt ps dealer)) ->
     model {table = TableModel (Map.fromList ps) (Just dealer)}
+  Right (PlayerTurnEvt (HitCard pid card)) ->
+    let playerHands = Map.adjust (addCard card) pid model.table.playerHands
+     in model {table = model.table {playerHands}}
   _ -> model
